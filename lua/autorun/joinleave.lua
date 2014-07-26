82e713263d08b890043229b7c9505f460c333a0d
therealist <schtherealist@hotmail.com>
2014-07-26 11:24:23 -0500

    Now shows number of players out of total.

--- lua/autorun/joinleave.lua
+++ lua/autorun/joinleave.lua
@@ -2,7 +2,7 @@ function fuserAuthed( ply )
 	
 		for k,v in pairs( player.GetAll() ) do
 		
-			ULib.tsayError( v, ply:Nick() .. " has joined the server." )
+			ULib.tsayError( v, ply:Nick() .. " has joined the server. " .. table.Count(player.GetAll()) .. "/30" )
 			
 		end
 			
@@ -13,7 +13,7 @@ function fPlayerDisconnect( ply )
 
 		for k,v in pairs( player.GetAll() ) do
 		
-			ULib.tsayError( v, ply:Nick() .. " has left the server." )
+			ULib.tsayError( v, ply:Nick() .. " has left the server. " .. table.Count(player.GetAll()) .. "/30" )
 							
 		end
 			
