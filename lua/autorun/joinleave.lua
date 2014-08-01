6dada02b808250e1e1328b046ddf9a390bdf60a2
therealist <schtherealist@hotmail.com>
2014-08-01 06:26:24 -0500

    Now with changeable colour

--- lua/autorun/joinleave.lua
+++ lua/autorun/joinleave.lua
@@ -2,7 +2,7 @@ function fuserAuthed( ply )
 	
 		for k,v in pairs( player.GetAll() ) do
 		
-			ULib.tsayError( v, ply:Nick() .. " has joined the server. " .. table.Count(player.GetAll()) .. "/30" )
+			ULib.tsayColor( v, false, Color( 255, 255, 0 ), ply:Nick() .. " has joined the server. " .. table.Count(player.GetAll()) .. "/30" )
 			
 		end
 			
@@ -13,7 +13,7 @@ function fPlayerDisconnect( ply )
 
 		for k,v in pairs( player.GetAll() ) do
 		
-			ULib.tsayError( v, ply:Nick() .. " has left the server. " .. table.Count(player.GetAll()) -1 .. "/30" )
+			ULib.tsayColor( v, false, Color( 255, 255, 0 ), ply:Nick() .. " has left the server. " .. table.Count(player.GetAll()) -1 .. "/30" )
 							
 		end
 			
