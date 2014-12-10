32183c83c0573d22768d193a6c8bc5e97a4a30da
The Realist <schtherealist@hotmail.com>
2014-12-10 15:57:21 -0600

    Now displays SteamID

--- lua/autorun/joinleave.lua
+++ lua/autorun/joinleave.lua
@@ -1,11 +1,11 @@
-function fuserAuthed( ply )
+function fplayerAuthed( ply )
 	
 		for k,v in pairs( player.GetAll() ) do
 			ULib.tsayColor( v, false, Color( 255, 255, 0 ), ply:Nick() .. " (" .. ply:SteamID() .. ")" .. " has joined the server. " .. table.Count(player.GetAll()) .. "/30" )
 		end
 			
 end
-hook.Add( "PlayerAuthed", "fauthed", fuserAuthed )
+hook.Add( "PlayerAuthed", "fauthed", fplayerAuthed )
 
 function fPlayerDisconnect( ply )
 
